dd1a4107783bbc1e60e8cdf79f16e47727c90103
kauefraga <kauefragarodrigues456@gmail.com>
2022-07-21 16:45:19 -0300

    :sparkles: feat: add app builder with cors, json parser and routes

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express, { Application } from 'express';
+import addApiRoutes from '@routes/index';
+
+function buildApp(): Application {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
+  addApiRoutes(app);
+
+  return app;
+}
+
+export default buildApp;
