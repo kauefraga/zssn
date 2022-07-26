67ca88c3e659f6065d27283f7418b383d3348ca2
kauefraga <kauefragarodrigues456@gmail.com>
2022-07-26 19:08:49 -0300

    :fire: chore: delete dir src/api

--- src/api/routes/register.ts
+++
@@ -1,8 +0,0 @@
-import RegisterController from '@controllers/register';
-import { Router } from 'express';
-
-const router = Router();
-
-router.post('/', RegisterController.handler);
-
-export default router;
--- src/api/routes/survivors.ts
+++
@@ -1,8 +0,0 @@
-import SurvivorsController from '@controllers/survivors';
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/', SurvivorsController.handler);
-
-export default router;
