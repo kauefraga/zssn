46312dd6176449a4c2949959142b9d1b59449c30
kauefraga <kauefragarodrigues456@gmail.com>
2022-07-26 22:20:17 -0300

    :wrench: config: remove routes from path mappers

--- jest.config.ts
+++ jest.config.ts
@@ -87,7 +87,6 @@ export default {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     "^@controllers/(.*)$": ["<rootDir>/src/controllers/$1"],
-    "^@routes/(.*)$": ["<rootDir>/src/routes/$1"],
     "^@middlewares/(.*)$": ["<rootDir>/src/middlewares/$1"]
   },
 
